95af469ffb1abfb500b63c03b61747579027aa0a
Gareth Hughes <gareth.hughes@nagra.com>
2024-10-27 12:06:18 +0000

    Add go

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "jdtls", "lua_ls", "rust_analyzer", "terraformls", "tsserver" },
+      ensure_installed = { "gopls", "jdtls", "lua_ls", "terraformls", "tsserver" },
     },
   },
   {
@@ -16,10 +16,10 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
+      lspconfig.gopls.setup({ capabilities = capabilities })
       lspconfig.jdtls.setup({ capabilities = capabilities })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-      lspconfig.terraformls.setup{}
+      lspconfig.terraformls.setup({})
       lspconfig.tsserver.setup({ capabilities = capabilities })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,23 +1,26 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        require("none-ls.diagnostics.eslint"),
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.google_java_format,
-        null_ls.builtins.formatting.nixpkgs_fmt,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.terraform_fmt,
-        null_ls.builtins.formatting.xmllint,
-      },
-    })
+		null_ls.setup({
+			sources = {
+				require("none-ls.diagnostics.eslint"),
+				null_ls.builtins.formatting.just,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.google_java_format,
+				null_ls.builtins.formatting.nixpkgs_fmt,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.terraform_fmt,
+				null_ls.builtins.formatting.xmllint,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
